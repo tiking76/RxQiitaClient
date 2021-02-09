0c87c54a051beb6d6f7caf31c595f4eb07cbd717
tiking76 <716yosiki1999@gmail.com>
2021-02-09 17:01:23 +0900

    :sparkles: add iOS project

---
+++ RxQiitaClient/RxQiitaClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxQiitaClient
+//
+//  Created by 舘佳紀 on 2021/02/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
